5ce45b7960e489e1a126e03632a5f4223206189f
xielbs <928135874@qq.com>
2019-02-19 17:03:30 +0800

    init

--- SpringCloud/ApiService/src/main/java/com/xlbs/apiservice/SwaggerConfig.java
+++ SpringCloud/ApiService/src/main/java/com/xlbs/apiservice/SwaggerConfig.java
@@ -26,10 +26,10 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
-                .contact("程序猿DD")
+                .title("ApiService 中的 APIs")
+                .description("更多相关文章请关注：http://blog.didispace.com/")
+//                .termsOfServiceUrl("http://blog.didispace.com/")
+                .contact("谢李宝生")
                 .version("1.0")
                 .build();
     }
