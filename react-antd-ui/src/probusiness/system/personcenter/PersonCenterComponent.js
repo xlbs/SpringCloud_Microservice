4d27a0a1122b313f037de3a3803ce7817681f65c
xlbs <928135874@qq.com>
2019-03-23 23:41:36 +0800

    init

--- react-antd-ui/src/probusiness/system/personcenter/PersonCenterComponent.js
+++ react-antd-ui/src/probusiness/system/personcenter/PersonCenterComponent.js
@@ -3,6 +3,27 @@ import Button from "antd/es/button/button";
 
 class PersonCenterComponent extends React.Component{
 
+    // componentWillMount() {
+    // }
+    // componentDidMount() {
+    //     console.log('Component DID MOUNT!')
+    // }
+    // componentWillReceiveProps(newProps) {
+    //     console.log('Component WILL RECEIVE PROPS!')
+    // }
+    // shouldComponentUpdate(newProps, newState) {
+    //     return true;
+    // }
+    // componentWillUpdate(nextProps, nextState) {
+    //     console.log('Component WILL UPDATE!');
+    // }
+    // componentDidUpdate(prevProps, prevState) {
+    //     console.log('Component DID UPDATE!')
+    // }
+    // componentWillUnmount() {
+    //     console.log('Component WILL UNMOUNT!')
+    // }
+
     render() {
 
         return (
