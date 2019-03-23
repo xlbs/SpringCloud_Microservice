9e8beae505cf8100bdd357220180d83e040cf89b
xlbs <928135874@qq.com>
2019-03-23 13:08:42 +0800

    init

--- react-antd-ui/src/reducers/index.js
+++ react-antd-ui/src/reducers/index.js
@@ -1,14 +1,18 @@
 import { combineReducers } from 'redux';
 import login from "../probusiness/login/LoginReducer";
 import main from "../probusiness/main/MainReducer";
-import home from "../probusiness/home/homeReducer";
-import personCenter from "../probusiness/system/personcenter/PersonCenterReducer";
+import home from "../probusiness/home/HomeReducer";
+import user from "../probusiness/system/user/UserReducer";
+import role from "../probusiness/system/role/RoleReducer";
+import menu from "../probusiness/system/menu/MenuReducer";
 
 const reducers = combineReducers({
     login,
     main,
     home,
-    personCenter
+    user,
+    role,
+    menu
 })
 
 export default reducers;
