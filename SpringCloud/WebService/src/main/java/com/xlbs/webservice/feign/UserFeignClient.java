41ab9fb0515ca46058296c9dd2bd512d6dfb768a
xlbs <928135874@qq.com>
2019-03-24 22:07:09 +0800

    init

--- SpringCloud/WebService/src/main/java/com/xlbs/webservice/feign/UserFeignClient.java
+++ SpringCloud/WebService/src/main/java/com/xlbs/webservice/feign/UserFeignClient.java
@@ -1,7 +1,6 @@
 package com.xlbs.webservice.feign;
 
 import com.xlbs.constantjar.ResponseResult;
-import com.xlbs.webservice.authentication.user.UserInfo;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
