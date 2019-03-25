6094f59bc92d917cd8db502d28717322188d53f8
xlbs <928135874@qq.com>
2019-03-25 23:20:49 +0800

    init

--- SpringCloud/WebService/src/main/java/com/xlbs/webservice/WebServiceApplication.java
+++ SpringCloud/WebService/src/main/java/com/xlbs/webservice/WebServiceApplication.java
@@ -1,15 +1,13 @@
 package com.xlbs.webservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
-@EnableEurekaClient //创建针对Eureka客户端的实例
 @EnableFeignClients
 @EnableZuulProxy
+@SpringCloudApplication
 public class WebServiceApplication {
 
     public static void main(String[] args) {
