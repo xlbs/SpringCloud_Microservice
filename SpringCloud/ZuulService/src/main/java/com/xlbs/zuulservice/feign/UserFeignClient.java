37153da1e940963621f5321dfe66f975fadab2f2
xielbs <928135874@qq.com>
2019-04-04 15:54:47 +0800

    init

--- SpringCloud/ZuulService/src/main/java/com/xlbs/zuulservice/feign/UserFeignClient.java
+++ SpringCloud/ZuulService/src/main/java/com/xlbs/zuulservice/feign/UserFeignClient.java
@@ -4,14 +4,14 @@ import com.xlbs.constantjar.RepStateCode;
 import com.xlbs.constantjar.ResponseResult;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.xlbs.constantjar.ServiceName;
 
 @FeignClient(value = ServiceName.API_SERVICE, fallback = UserFeignFallback.class)
 public interface UserFeignClient {
 
-    @RequestMapping(value = "/user/login")
+    @GetMapping(value = "/signIn")
     ResponseResult findUserByUsername(@RequestParam("username") String username);
 
 }
