c39a1779c800940e6de1af093e8f9fe02a3cf000
xielbs <928135874@qq.com>
2019-04-08 17:15:52 +0800

    init

--- react-antd-ui/src/commutils/components/CreateTable.js
+++ react-antd-ui/src/commutils/components/CreateTable.js
@@ -5,10 +5,10 @@ function formatDate(timestamp) {
     const date = new Date(timestamp);
     const year = date.getFullYear() + '-';
     const month = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-    const day = date.getDate() + ' ';
-    const hour = date.getHours() + ':';
-    const minute = date.getMinutes() + ':';
-    const second = date.getSeconds();
+    const day = (date.getDate() < 10 ? '0'+date.getDate() : date.getDate()) +' ';
+    const hour = (date.getHours() < 10 ? '0'+date.getHours() : date.getHours()) + ':';
+    const minute = (date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()) + ':';
+    const second = (date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds());
     return year+month+day+hour+minute+second;
 }
 
