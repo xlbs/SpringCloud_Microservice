a8c591e8bb1e73de928667b04557cefd0931dfab
xielbs <928135874@qq.com>
2019-05-13 15:28:43 +0800

    init

--- SpringCloud/ApiService/src/main/java/com/xlbs/apiservice/ApiServiceApplication.java
+++ SpringCloud/ApiService/src/main/java/com/xlbs/apiservice/ApiServiceApplication.java
@@ -2,11 +2,13 @@ package com.xlbs.apiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCaching
 @ComponentScan(value = "com.xlbs.commutils")
 @ComponentScan(value = "com.xlbs.apiservice")
 public class ApiServiceApplication {
